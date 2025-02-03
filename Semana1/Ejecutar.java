79d559e83172030d85ba149bd1256783192f861f
Eidan07 <eidan.zuniga@campusucc.edu.co>
2025-02-03 14:21:14 +0000

    Se agregó la palabra JAVA

--- Semana1/Ejecutar.java
+++ Semana1/Ejecutar.java
@@ -2,7 +2,7 @@ public class Ejecutar {
     public static void main(String[] args){
 
         //Mostrar por pantalla
-        System.out.println("Hola Mundo!!!");
+        System.out.println("Hola Mundo!!! JAVA ");
 
     }
 }
