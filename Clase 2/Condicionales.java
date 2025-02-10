4a7113d36eff76e9364dc7e67dfc7ea84a4fbdf6
Eidan07 <eidan.zuniga@campusucc.edu.co>
2025-02-10 13:59:02 +0000

    Se creo nuevamente el archivo

--- Clase 2/Condicionales.java
+++ Clase 2/Condicionales.java
@@ -1,12 +1,12 @@
 public class Condicionales {
-    public static void main (String []args){
+    public static void main (String[] args){
         int numero = -5;
         if (numero > 0){
             System.out.println("El número es positivo");
-        }else if (numero <0){
+        }else if (numero < 0){
             System.out.println("El número es negativo");
         }else{
-            System.out.println(("El número es cero"));
+            System.out.println("El número es cero");
         }
         }
     
