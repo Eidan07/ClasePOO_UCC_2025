68f9d604aad18ce9862d45286e5aa30b3721cc45
Eidan07 <eidan.zuniga@campusucc.edu.co>
2025-02-10 14:44:13 +0000

    Se creo un bucle para que imprima un mensaje

--- Clase 2/Bucles.java
+++ Clase 2/Bucles.java
@@ -1,5 +1,9 @@
 public class Bucles {
     public static void main (String[] args){
-        
+
+        //
+        for(int i = 1; i <= 5; i++){
+            System.out.println(i + ": Pogramación Orientaba a Objetos");
+        }
     }
 }
