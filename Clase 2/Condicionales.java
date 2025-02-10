47fa01fbec21166f5bb7131665613171c9522c55
Eidan07 <eidan.zuniga@campusucc.edu.co>
2025-02-10 13:54:31 +0000

    Cambio de nombre

--- Clase 2/Condicionales.java
+++ Clase 2/Condicionales.java
@@ -1,3 +1,13 @@
-public class Condicionales {
+public class condi {
+    public static void main (String []args){
+        int numero = -5;
+        if (numero > 0){
+            System.out.println("El número es positivo");
+        }else if (numero <0){
+            System.out.println("El número es negativo");
+        }else{
+            System.out.println(("El número es cero"));
+        }
+        }
     
 }
