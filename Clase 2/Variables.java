9df518b63eee0af36ec5b38605378b15db961b99
Eidan07 <eidan.zuniga@campusucc.edu.co>
2025-02-10 13:43:53 +0000

    Se reorganizaron los archivos para mas comodidad

---
+++ Clase 2/Variables.java
@@ -0,0 +1,25 @@
+public class Variables{
+    public static void main(String[]args){
+        
+        //Declaración de una variable de tipo entero
+        int edad = 25;
+        
+        //Declaración de una variable de tipo real.
+        double altura = 1.75;
+        
+        //Declaración de una variable de tipo caden
+        String nombre = "Eidan";
+        
+        //Declaración de una variable de tipo boolean
+        boolean esEstudiante = true;
+        
+        /* 
+        Con la instruccion System.out.println se mostraran
+        Las variables y cada una de ellas estará concatenadas con el signo (+)
+        y la leyenda. 
+        */
+        System.out.println("Nombre: " + nombre + ",Edad: " + edad + ", Altura: " + altura);
+        System.out.println("Estudiante" + esEstudiante);
+
+    }
+}
