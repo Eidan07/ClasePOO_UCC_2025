23da6fff3b80783926c669c1dac39f4ad369772e
Eidan07 <eidan.zuniga@campusucc.edu.co>
2025-02-17 12:53:32 +0000

    Arerglos en java

---
+++ Clase 3/Arreglos.java
@@ -0,0 +1,13 @@
+public class Arrelgos{
+    public static void main(String[] args){
+        /*
+         *Primera forma de declarar un arreglo
+         *int[] a = {8, 3, 10, 22, 1, 9};
+         *
+         * Segunda forma de declarar un arreglo
+         * int[] a = new int[6];
+         * E número ahce referencia al tamaño del arreglo
+         * tendremos que llenar el arreglo
+         * /
+    }
+}
