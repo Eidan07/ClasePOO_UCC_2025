18263ca5bed325e9400a8c0fbc8a8607625fa2d8
Eidan07 <eidan.zuniga@campusucc.edu.co>
2025-02-24 13:09:13 +0000

    Se crearon archivos para los ejerciciso del taller

---
+++ EjerciciosPOO202510.java/Ejercicio11.java
@@ -0,0 +1,17 @@
+public class Ejercicio3{
+    public static void main (String[] args){
+        /* Dado un arreglo A de N elementos se desea generar tres arreglos 
+        que contengan los elementos negativos, cero y positivos de arreglo inicial. */
+        //Creacion del arreglo a de enteros n elementos
+      
+        //declaración del arreglo del tamaño
+        
+        int n = (int) (Math.random() * (30 + 15)) + 15;
+        int[] a = new int[n];
+        
+        //Llenar el arreglo con numeros comprendidos entre -2' y 2'
+        for(int i = 0; i < a.length; i++){
+            a[i] = (int) (Math.random() * (20 + (-20))) + (-20);
+        }   
+    }
+}
