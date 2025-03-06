69a0270406a2181a7706a43df8412e04b8553853
Eidan07 <eidan.zuniga@campusucc.edu.co>
2025-03-06 23:56:46 +0000

    Momento 1

---
+++ Clase 4/ParkingNow.java
@@ -0,0 +1,10 @@
+ public class ParkingNow {
+    public static void main(String[] args){
+        //Primero se da la vienvenida al Usuario
+        System.out.println("Bienvenido a ParkingNow");
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Por favor digite la placa de la moto: ");
+        int n = leer.nextInt();
+        
+    }
+ }
