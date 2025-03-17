79df66a6d2128479c97e5b0b04835b7f0f5ab632
Eidan07 <eidan.zuniga@campusucc.edu.co>
2025-03-17 14:03:04 +0000

    Creacion de los objetos en la clase ejecutar

---
+++ Clase 7/Ejecutar.java
@@ -0,0 +1,9 @@
+public class Ejecutar {
+    public static void main(String[] args) {
+
+        // Instanciar Creación del Objeto (Creación)
+        Estudiante objEst1 = new Estudiante(1, "Eidan", "Zuñiga", 18, "Ingienería");
+        Estudiante objEst2 = new Estudiante(2, "Juan", "Castro", 20, "Derecho");
+
+    }
+}
