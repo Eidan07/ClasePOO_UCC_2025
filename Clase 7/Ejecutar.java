43810d9a3e4be70666246fa8532203a71c0fb177
Eidan07 <eidan.zuniga@campusucc.edu.co>
2025-03-17 14:31:24 +0000

    Cambió de edad en la clase ejecutar

--- Clase 7/Ejecutar.java
+++ Clase 7/Ejecutar.java
@@ -5,5 +5,12 @@ public class Ejecutar {
         Estudiante objEst1 = new Estudiante(1, "Eidan", "Zuñiga", 18, "Ingienería");
         Estudiante objEst2 = new Estudiante(2, "Juan", "Castro", 20, "Derecho");
 
+        System.out.println(objEst1.toString()); 
+        System.out.println(objEst2); //toString
+
+        //Modificar la edad del estudiante
+        objEst1.setEdad(20);
+
+        System.out.println(objEst1);
     }
 }
