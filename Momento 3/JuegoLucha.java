83b3b00a8f94c25e0ff5c21b9dc935099d84c845
Eidan07 <eidan.zuniga@campusucc.edu.co>
2025-05-12 05:15:55 +0000

    Se creo el Momento 3

---
+++ Momento 3/JuegoLucha.java
@@ -0,0 +1,35 @@
+public class JuegoLucha {
+    private Personaje jugador1;
+    private Personaje jugador2;
+
+    // Constructor
+    public JuegoLucha(String nombre1, String nombre2) {
+        jugador1 = new Personaje(nombre1);
+        jugador2 = new Personaje(nombre2);
+    }
+
+    // Método para iniciar la pelea
+    public void iniciarPelea() {
+        System.out.println("La pelea comienza entre " + jugador1.getNombre() + " y " + jugador2.getNombre() + "...");
+
+        while (jugador1.estaVivo() && jugador2.estaVivo()) {
+            turno(jugador1, jugador2);
+            if (jugador2.estaVivo()) {
+                turno(jugador2, jugador1);
+            }
+        }
+
+        if (jugador1.estaVivo()) {
+            System.out.println(jugador1.getNombre() + " ha ganado la pelea.");
+        } else {
+            System.out.println(jugador2.getNombre() + " ha ganado la pelea.");
+        }
+    }
+
+    // Método que representa un turno de ataque
+    private void turno(Personaje atacante, Personaje defensor) {
+        System.out.println("Turno de " + atacante.getNombre() + ". Puntos de vida de " + defensor.getNombre() + ": " + defensor.getPuntosDeVida());
+        atacante.atacar(defensor);
+        System.out.println(defensor.getNombre() + " ahora tiene " + defensor.getPuntosDeVida() + " puntos de vida.");
+    }
+}
